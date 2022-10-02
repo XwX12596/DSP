2bc282b60e5f2255b3f561b044c5b0c8c510eede
XwX12596 <1259685860@qq.com>
2022-10-02 18:06:04 +0800

    ipynb die

--- proj3/codes/ML.py
+++ proj3/codes/ML.py
@@ -1,32 +1,46 @@
-#!/usr/bin/env python
-# coding: utf-8
-
+# %%
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+
 from matplotlib import pyplot
 
+
+# %% [markdown]
+# Use pandas to read .csv files
+
+# %%
 train = pd.read_csv("./train.csv")
+
+# %% [markdown]
+# Devide into input and output
+
+# %%
 y = train["method"]
 x = train.drop(['method','No'],axis=1)
 
+# %% [markdown]
+# training
 
-# In[7]:
-
-
-# from sklearn.neighbors import KNeighborsClassifier
-# knn = KNeighborsClassifier()
-# knn.fit(x,y)
+# %%
 from sklearn.tree import DecisionTreeClassifier
 tree = DecisionTreeClassifier()
 tree.fit(x,y)
 
+# %% [markdown]
+# test for myself, I may need some help....
 
-# In[9]:
-
-
+# %%
 test = pd.read_csv("test.csv")
 ans = tree.predict(test)
 ans
+
+# %% [markdown]
+# We can find that the darker the color of the grid, the stronger the correlation between the two labels.
+
+# %%
+plt.figure(figsize = (20,8))
+sns.heatmap(train.corr(), cmap = 'Blues', annot = True)
+
 
