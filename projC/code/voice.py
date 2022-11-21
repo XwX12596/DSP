73527b569bdbb0142c7e94d3200b0396d17f16ef
XwX12596 <1259685860@qq.com>
2022-11-21 20:39:15 +0800

    Project C ver.1

--- projC/code/voice.py
+++ projC/code/voice.py
@@ -4,8 +4,8 @@ from matplotlib import pyplot as plt
 from scipy.io.wavfile import write, read
 
 """Read Record File"""
-fs, x = read('short.wav')
-x = x[:, 0]
+fs, x = read('./projC/code/short.wav')
+# x = x[:, 0]
 sec = x.size/fs
 t = np.arange(0, sec, 1/fs)
 
@@ -26,18 +26,18 @@ xt[:int(fl * sec)] = 0
 
 """Do IFFT And Output"""
 ix = np.array(np.real(ifft(xt)), dtype=np.int16)
-write('out-voice.wav', fs, ix)
+write('./projC/code/out-voice.wav', fs, ix)
 
 """Draw Wave and Spectrum"""
-ax0 = plt.subplot(311)
-ax0.set_title("Original Wave")
-ax0.plot(t, x)
+# ax0 = plt.subplot(311)
+# ax0.set_title("Original Wave")
+# ax0.plot(t, x)
 
-ax1 = plt.subplot(312) 
+ax1 = plt.subplot(211) 
 ax1.set_title("Wave Through Filter")
 ax1.plot(t, ix)
 
-ax2 = plt.subplot(313)
+ax2 = plt.subplot(212)
 ax2.set_title("Spectrum of Wave Through Filter")
 ax2.plot(freq, np.abs(xt))
 
